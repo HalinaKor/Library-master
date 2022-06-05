c885774763e2f7aa1d9b5837faf84d4caa9217f7
HalinaKor <gali_korolyova@mail.ru>
2022-06-05 20:02:50 +0300

    Spring Security

---
+++ src/main/java/by/company/library/configuration/MyUserDetails.java
@@ -0,0 +1,56 @@
+package by.company.library.configuration;
+
+import by.company.library.domain.dbo.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MyUserDetails implements UserDetails {
+
+    private UserEntity userEntity;
+
+    public MyUserDetails(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getName();
+    }
+
+    public String getPassportNo() {
+        return userEntity.getPassportNo();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/by/company/library/configuration/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package by.company.library.configuration;
+
+import by.company.library.domain.dbo.UserEntity;
+import by.company.library.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String passportNo)
+            throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByPassportNo(passportNo);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+}
